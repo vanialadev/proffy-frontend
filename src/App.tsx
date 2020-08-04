658a477946936b842f19358f334f4fe7d892f5cd
Vânia Almeida <vania.almeida28@hotmail.com>
2020-08-04 01:05:33 -0300

    tsx da pagina landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './assets/styles/global.css';
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Landing />
   );
 }
 
