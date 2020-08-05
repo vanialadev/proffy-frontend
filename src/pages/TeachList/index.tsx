167259d93c387edf4704cf274b2c29743093e86a
Vânia Almeida <vania.almeida28@hotmail.com>
2020-08-05 00:57:59 -0300

    criando o header da pagina teach list

--- src/pages/TeachList/index.tsx
+++ src/pages/TeachList/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
+
+import './styles.css';
 
 function TeachList() {
-  return <h1>Teach List</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Voltar"/>
+          </Link>
+          <img src={logoImg} alt="logo Proffy"/>
+        </div>
+
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis</strong>
+        </div>
+      </header>
+    </div>
+  );
+  
 }
 
 export default TeachList;
