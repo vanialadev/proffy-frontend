b896f19a477e2e4f6da533fd3c7f2433df2314f7
Vânia Almeida <vania.almeida28@hotmail.com>
2020-08-05 04:42:07 -0300

    criando as informacoes do professor

--- src/pages/TeachList/index.tsx
+++ src/pages/TeachList/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import "./styles.css";
 
 import PageHeader from "../../components/PageHeader";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
 
 function TeachList() {
   return (
@@ -22,6 +24,40 @@ function TeachList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars2.githubusercontent.com/u/22610714?s=460&u=2c96ab4929dc4fa9f0a00f9585981913f3e47ee6&v=4"
+              alt="imagem do professor"
+            />
+            <div>
+              <strong>Vania Almeida </strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br />
+            <br />
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das
+            pessoas através de experiências. Mais de 200.000 pessoas já passaram
+            por uma das minhas explosões.
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora<strong>R$ 80,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="whatsapp icone" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
